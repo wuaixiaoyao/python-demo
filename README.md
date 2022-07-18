4cb7db8255dd3ba7763c21c9c3c7eefb8aa93498
yll <837865811@qq.com>
2022-07-18 23:35:14 +0800

    📃 docs(readme ): 添加参考

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 <!--
  * @Author: yll yangll-h@glodon.com
  * @Date: 2022-07-14 22:02:31
- * @LastEditors: yll yangll-h@glodon.com
- * @LastEditTime: 2022-07-14 22:02:39
+ * @LastEditors: yll 837865811@qq.com
+ * @LastEditTime: 2022-07-18 23:34:34
  * @FilePath: /composetest/README.md
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 -->
-# python demo
+# python demo1
+
+### 参考
+
+- [python 教程](https://www.liaoxuefeng.com/wiki/1016959663602400/1018491264935776)
--- app.py
+++ app.py
@@ -1,12 +1,3 @@
-'''
-Author: yll yangll-h@glodon.com
-Date: 2020-08-18 19:32:17
-LastEditors: yll yangll-h@glodon.com
-LastEditTime: 2022-07-14 21:39:43
-FilePath: /composetest/app.py
-Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
-'''
-
 import time
 import redis
 from flask import Flask
@@ -17,13 +8,11 @@ app = Flask(__name__)
 cache = redis.Redis(host='redis', port=6379)
 
 
-
 @app.route('/')
 def heme():
     count = get_hit_count()
     name = 'yll'
     return 'Hello World! I {} have been seen {} times.\n'.format(name, count)
-
 
 
 @app.route('/signin', methods=['GET'])
@@ -34,16 +23,17 @@ def signin_form():
               <p><button type="submit">Sign In</button></p>
               </form>'''
 
+
 @app.route('/signin', methods=['POST'])
 def signin():
-    # 需要从request对象读取表单内容：
-    if request.form['username']=='admin' and request.form['password']=='password':
+    if request.form['username'] == 'admin' and request.form['password'] == 'password':
         return '<h3>Hello, admin!</h3>'
     return '<h3>Bad username or password.</h3>'
 
+
 if __name__ == '__main__':
     app.run()
-    
+
 
 def get_hit_count():
     retries = 5
@@ -55,5 +45,3 @@ def get_hit_count():
                 raise exc
             retries -= 1
             time.sleep(0.5)
-
-
